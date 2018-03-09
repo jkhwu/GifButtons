2a3cab2be8343bcb72931db94e856958500b0e78
Janet Hwu <Janet@MacBook-Pro-2.local>
2018-03-09 01:16:00 -0800

    finished search submit functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,34 @@
 "use strict";
 (() => {
     const app = {
+
         btns: ["ballet", "swing", "blues", "jazz", "tango", "foxtrot", "waltz", "moonwalk", "break", "interpretive", "dog", "hula"],
+
         colors: ["red", "orange", "yellow", "olive", "green", "blue", "violet", "purple", "pink", "brown"],
+
         load: () => {
-            app.makeBtns();
-            $("#btnZone > .button").on("click", () => app.getGifs());
+            app.makeBtns(app.btns);
+            $("#submit").on("click", (event) => {
+                event.preventDefault();
+                app.addBtn($("#searchTerm").val());
+                $("#searchTerm").val("");
+            });
+            $("#btnZone").on("click", ".danceBtn", function() { console.log($(this).val()) });
         },
-        makeBtns: () => app.btns.forEach(dance => { $("#btnZone").append(`<button class="ui inverted ${app.randColor()} button" value="${dance}">${dance}</button>`) }),
+
+        makeBtns: (arr) => arr.forEach(dance => app.addBtn(dance)),
+
+        addBtn: (text) => $("#btnZone").append(`<button class="ui inverted ${app.randColor()} button danceBtn" value="${text}">${text}</button>`),
+
         randColor: () => app.colors[Math.floor(Math.random() * app.colors.length)],
-        getGifs: () => {
+
+        getGifs: (btn) => {
             // $.ajax({ url: "https://api.giphy.com/v1/gifs/search?api_key=qZKo14UVEsTDJkz1YpxVkj9cq7HxPC1N&limit=12&lang=en&q=dance" + $("input").val(), method: "GET" }).then(response => app.makeStills(response));
-            console.log($(this).val())
+            console.log(btn);
         },
+
         makeStills: (res) => res.data.forEach((element) =>
-            $("#gifZone").append(`
-            <div class="ui card">
-                <div class="image">
-                    <img src=${element.images.original_still.url}>
-                    <h2><i class="play circle outline icon"></i></h2>
-                </div>
-                <div class="content">
-                    <div class="extra content"> Rating: </div>
-                </div>
-            </div>`)
+            $("#gifZone").append(`<div class="ui medium rounded image"><a href="#" class="ui red ribbon label">Rating</a><img src=${element.images.original_still.url}><h2><i class="play circle outline icon"></i></h2></div>`)
         )
     }
     app.load();
