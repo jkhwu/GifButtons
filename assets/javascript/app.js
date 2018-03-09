804a620a187a94a4cf5526329a05503c21cd1bda
Janet Hwu <Janet@MacBook-Pro-2.local>
2018-03-09 03:10:41 -0800

    added favicon and cursor, finalized functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,34 +2,42 @@
 (() => {
     const app = {
 
-        btns: ["ballet", "swing", "blues", "jazz", "tango", "foxtrot", "waltz", "moonwalk", "break", "interpretive", "dog", "hula"],
+        btns: ["ballet", "blues", "carlton", "hula", "moonwalk", "swing", "tango", "waltz"],
 
         colors: ["red", "orange", "yellow", "olive", "green", "blue", "violet", "purple", "pink", "brown"],
 
         load: () => {
             app.makeBtns(app.btns);
-            $("#submit").on("click", (event) => {
+            $("#submit").on("click", event => {
                 event.preventDefault();
                 app.addBtn($("#searchTerm").val());
                 $("#searchTerm").val("");
             });
-            $("#btnZone").on("click", ".danceBtn", function() { console.log($(this).val()) });
+            $("#btnZone").on("click", ".danceBtn", app.getGifs);
+            $("#gifZone").on("click", ".gif", app.playGif);
         },
 
-        makeBtns: (arr) => arr.forEach(dance => app.addBtn(dance)),
+        makeBtns: arr => arr.forEach(dance => app.addBtn(dance)),
 
-        addBtn: (text) => $("#btnZone").append(`<button class="ui inverted ${app.randColor()} button danceBtn" value="${text}">${text}</button>`),
+        addBtn: text => $("#btnZone").append(`<button class="ui inverted ${app.randColor()} button danceBtn" value="${text}">${text}</button>`),
 
         randColor: () => app.colors[Math.floor(Math.random() * app.colors.length)],
 
-        getGifs: (btn) => {
-            // $.ajax({ url: "https://api.giphy.com/v1/gifs/search?api_key=qZKo14UVEsTDJkz1YpxVkj9cq7HxPC1N&limit=12&lang=en&q=dance" + $("input").val(), method: "GET" }).then(response => app.makeStills(response));
-            console.log(btn);
+        getGifs: function() {
+            $.ajax(`https://api.giphy.com/v1/gifs/search?api_key=qZKo14UVEsTDJkz1YpxVkj9cq7HxPC1N&limit=12&lang=en&q=${$(this).val()}+dance`).then(response => app.makeStills(response));
+            $(this).addClass("active");
         },
 
-        makeStills: (res) => res.data.forEach((element) =>
-            $("#gifZone").append(`<div class="ui medium rounded image"><a href="#" class="ui red ribbon label">Rating</a><img src=${element.images.original_still.url}><h2><i class="play circle outline icon"></i></h2></div>`)
-        )
+        makeStills: res => {
+            $("#gifZone").empty();
+            res.data.forEach(gif => { $("#gifZone").append(`<div class="ui medium rounded image gif"><a href="${gif.images.original.url}" class="ui ${app.randColor()} ribbon label"  target="_blank">${gif.rating.toUpperCase()}</a><img src=${gif.images.original_still.url}></div>`) });
+        },
+
+        playGif: function() {
+            let url = $(this).children("img").attr("src");
+            console.log("play/pause");
+            url.endsWith("_s.gif") ? $(this).children("img").attr("src", url.replace("_s.gif", ".gif")) : $(this).children("img").attr("src", url.replace(".gif", "_s.gif"));
+        }
     }
     app.load();
 })();
