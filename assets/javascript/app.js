cfcf4022ba47c7b4dd8fc7abadb60bdb667fd61c
Janet Hwu <Janet@MacBook-Pro-2.local>
2018-03-09 04:04:43 -0800

    finished app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,29 @@
 "use strict";
 (() => {
     const app = {
-
-        btns: ["ballet", "blues", "carlton", "hula", "moonwalk", "swing", "tango", "waltz"],
-
+        btns: ["argentine tango", "b-boy", "ballet", "belly", "bhangra", "carlton", "charleston", "disco", "dougie", "hip-hop", "hula", "gangnam style", "lindy hop", "line", "moonwalk", "paso doble", "salsa", "shimmy", "waltz"],
         colors: ["red", "orange", "yellow", "olive", "green", "blue", "violet", "purple", "pink", "brown"],
-
         load: () => {
             app.makeBtns(app.btns);
             $("#submit").on("click", event => {
                 event.preventDefault();
-                app.addBtn($("#searchTerm").val());
+                app.addBtn($("#searchTerm").val().toLowerCase());
                 $("#searchTerm").val("");
             });
             $("#btnZone").on("click", ".danceBtn", app.getGifs);
             $("#gifZone").on("click", ".gif", app.playGif);
         },
-
         makeBtns: arr => arr.forEach(dance => app.addBtn(dance)),
-
         addBtn: text => $("#btnZone").append(`<button class="ui inverted ${app.randColor()} button danceBtn" value="${text}">${text}</button>`),
-
         randColor: () => app.colors[Math.floor(Math.random() * app.colors.length)],
-
         getGifs: function() {
-            $.ajax(`https://api.giphy.com/v1/gifs/search?api_key=qZKo14UVEsTDJkz1YpxVkj9cq7HxPC1N&limit=12&lang=en&q=${$(this).val()}+dance`).then(response => app.makeStills(response));
+            $.ajax(`https://api.giphy.com/v1/gifs/search?api_key=qZKo14UVEsTDJkz1YpxVkj9cq7HxPC1N&limit=10&lang=en&q=${$(this).val()}+dance`).then(response => app.makeStills(response));
             $(this).addClass("active");
         },
-
         makeStills: res => {
             $("#gifZone").empty();
             res.data.forEach(gif => { $("#gifZone").append(`<div class="ui medium rounded image gif"><a href="${gif.images.original.url}" class="ui ${app.randColor()} ribbon label"  target="_blank">${gif.rating.toUpperCase()}</a><img src=${gif.images.original_still.url}></div>`) });
         },
-
         playGif: function() {
             let url = $(this).children("img").attr("src");
             console.log("play/pause");
